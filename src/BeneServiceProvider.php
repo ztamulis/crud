7f3eeff2c50ac9e8edc6a6c63d93a4f99877d76f
Zygintas Tamulis <zygintas.tamulis@gmail.com>
2018-07-22 19:06:23 +0300

    Initial commit.

---
+++ src/BeneServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bene\Package;
+
+use Illuminate\Support\ServiceProvider;
+
+class BeneServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        include __DIR__.'/routes.php';
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,4 @@
+<?php
+Route::get('package', function(){
+    echo 'Hello from the bene package!';
+});
